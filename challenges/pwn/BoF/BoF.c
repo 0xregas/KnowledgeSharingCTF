f58b68d2321ba5ef0b7913e5f9466be2efcc8428
0xregas <jmregateiro@gmail.com>
2024-08-21 22:31:48 +0100

    finish BoF challenge

--- challenges/pwn/BoF/BoF.c
+++ challenges/pwn/BoF/BoF.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+
+void printFlag(){
+
+	FILE *flagFile = fopen("flag.txt", "r");
+	
+	//Allocate buffer size to read from file
+	size_t buff_size = 1024;
+	char *buff = (char *)malloc(buff_size);
+
+	//Read the file contents into the buffer
+	size_t bytes_read = fread(buff, 1, buff_size, flagFile);
+	if (bytes_read != 0) {
+		puts("Here is your flag: ");
+		printf("%s", buff); //print content of the buffer/file
+	}
+
+	fclose(flagFile); //close file
+	free(buff); //free memory occupied by the buffer
+}
 
 int main(){
 
-	char name[16];
-	char changeme[] = "flagProtector"
+	char name[8];
+	char changeme[8] = "hello";
 
-	if(strcmp(changeme, "flagProtector"))
+	printf("Enter your name: ");
+	gets(name);
+
+	if(strcmp(changeme, "hello") != 0)
 	{
-		printf('Goodbye, %s', name)
+		printFlag();
+		//hacked();
 	} else {
-		//return flag
+		printf("Goodbye, %s \n", name);
 	}
 
 	return 0;
