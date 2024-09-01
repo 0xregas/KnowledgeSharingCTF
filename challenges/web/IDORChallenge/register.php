216e4099810c5f0e52e76969ecc696bf44944bd2
0xregas <jmregateiro@gmail.com>
2024-09-01 23:27:29 +0100

    Added database connection

---
+++ challenges/web/IDORChallenge/register.php
@@ -0,0 +1,22 @@
+<?php
+
+
+	require('connect.php');
+	if (isset($_POST['username']) && isset($_POST['passw'])){
+		$user = $_POST['username'];
+		$userpass = $_POST['passw'];
+
+		$query = "INSERT INTO users (username, password) VALUES ('$user', '$userpass')";
+		#$query = "INSERT INTO users (username, password) VALUES ('teste', 'teste')";
+		$result = mysqli_query($conn, $query);
+
+		if ($result){
+			echo "User created successfully";
+		}
+		else {
+			echo "User not added";
+		}
+	}
+
+
+?>
