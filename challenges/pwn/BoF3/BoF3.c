15b3e22a29dcfd48c6bfc9d15bf42c0271570c0a
0xregas <jmregateiro@gmail.com>
2024-10-09 23:03:25 +0100

    BoF2 and 3 added

---
+++ challenges/pwn/BoF3/BoF3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void printFlag(){
+
+	FILE *flagFile = fopen("flag.txt", "r");
+	
+	//Allocate buffer size to read from file
+	size_t buff_size = 1024;
+	char *buff = (char *)malloc(buff_size);
+
+	//Read the file contents into the buffer
+	size_t bytes_read = fread(buff, 1, buff_size, flagFile);
+	if (bytes_read != 0) {
+		puts("Here is your flag: ");
+		printf("%s", buff); //print content of the buffer/file
+	}
+
+	fclose(flagFile); //close file
+	free(buff); //free memory occupied by the buffer
+}
+
+
+int main(){
+	
+	char name[8];
+	int admin = 0x123456789;
+
+	printf("Enter your name: \n");
+	gets(name);
+
+	if(admin == 0xdeadbeef)
+	{
+		printFlag();
+	} else {
+		printf("Goodbye, %s \n", name);
+	}
+
+	return 0;
+}
